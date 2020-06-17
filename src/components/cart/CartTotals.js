c508bb88b3a938aa9a1945560765d15fda7ed995
alexandernenov <alexander.nenov@mentormate.com>
2020-06-17 10:10:57 +0300

    user verify to complete order

--- src/components/cart/CartTotals.js
+++ src/components/cart/CartTotals.js
@@ -3,20 +3,27 @@ import PropTypes from 'prop-types';
 import {Card, Button} from 'react-bootstrap';
 import ProductPrice from '../product/ProductPrice';
 import {completeOrder} from '../../actions/cartActions';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 function CartTotals(props) {
     const dispatch = useDispatch();
+    const loggedIn = useSelector(state => state.userAuth.loggedIn);
 
     function handleClick() {
-        dispatch(completeOrder(props.items))
+        loggedIn && dispatch(completeOrder(props.items))
     }
 
     return (
         <Card>
             <h5>Cart Totals:</h5>
             <p>Total : <ProductPrice price={props.total} /></p>
-            <Button onClick={handleClick}>Complete order</Button>
+            {!loggedIn && 
+                <div>
+                    <p className="text-warning">You need to be logged in to complete an order</p>
+                    <a href="/login">Login here</a>
+                </div>
+            }
+            <Button onClick={handleClick} disabled={!loggedIn}>Complete order</Button>
         </Card>
     )
 }
