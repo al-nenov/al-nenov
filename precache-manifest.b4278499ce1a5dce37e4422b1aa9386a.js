06be9b89b2462938ddf6d6ac54d4bf33940743e5
alexandernenov <alexander.nenov@mentormate.com>
2020-06-18 18:20:02 +0300

    Updates

---
+++ precache-manifest.b4278499ce1a5dce37e4422b1aa9386a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf4b2c7b31f9bf7c06db6818bcd311b",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "fde62c698043004ac76e",
+    "url": "/react-shop/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "e0ce002e9c7d64d27dd1",
+    "url": "/react-shop/static/css/main.3f3eeece.chunk.css"
+  },
+  {
+    "revision": "fde62c698043004ac76e",
+    "url": "/react-shop/static/js/2.ee85ab5f.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/react-shop/static/js/2.ee85ab5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ce002e9c7d64d27dd1",
+    "url": "/react-shop/static/js/main.6829f217.chunk.js"
+  },
+  {
+    "revision": "b61441eb5b433fb5d336",
+    "url": "/react-shop/static/js/runtime-main.0f84c844.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d37c9328bd9335fe66cdf13ce2522380.js"
+  "/react-shop/precache-manifest.b4278499ce1a5dce37e4422b1aa9386a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-shop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
