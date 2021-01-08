0809673567593b040f5de31c2aa7c0dd3a0a67d2
Alexander Nenov <al.nenov@gmail.com>
2021-01-08 21:32:30 +0200

    Cart actions refactor

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,28 +1,15 @@
-import {CART_TYPES} from './cart.types';
+import { CART_TYPES } from './cart.types'
 
-export function addToCart(cartItems) {
-    return function(dispatch) {
-        dispatch({
-            type: CART_TYPES.ADD_PRODUCT_TO_CART,
-            payload: cartItems
-        })
-    }
-}
+export const addToCart = (cartItems) => ({
+    type: CART_TYPES.ADD_PRODUCT_TO_CART,
+    payload: cartItems
+})
 
-export function removeFromCart(itemId) {
-    return function(dispatch) {
-        dispatch({
-            type: CART_TYPES.REMOVE_ITEM_FROM_CART,
-            payload: itemId 
-        })
-    }
-}
+export const removeFromCart = (itemId) => ({
+    type: CART_TYPES.REMOVE_ITEM_FROM_CART,
+    payload: itemId
+})
 
-
-export function emptyCart() {
-    return function(dispatch) {
-        dispatch({
-            type: CART_TYPES.EMPTY_CART
-        })
-    }
-}
+export const emptyCart = () => ({
+    type: CART_TYPES.EMPTY_CART
+})
