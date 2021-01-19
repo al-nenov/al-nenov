27a6ce90c033f2d15cea194e666d312e97f54f00
Alexander Nenov <al.nenov@gmail.com>
2021-01-19 14:55:21 +0200

    Login and signup forms reworked with material ui

--- src/pages/login/login-page.component.jsx
+++ src/pages/login/login-page.component.jsx
@@ -1,22 +1,105 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import LoginForm from '../../components/forms/login-form.component'
-import { Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { loginUser } from '../../redux/user/user.actions'
 
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import { useStyles } from './login-page.styles'
+import { FormHelperText } from '@material-ui/core'
+
 const Login = () => {
+    const classes = useStyles()
+    const error = useSelector((state) => state.userAuth.error)
+
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: ''
+    })
+
+    const { email, password } = credentials
+
     const dispatch = useDispatch()
-    const login_user = (emailAndPassword) => dispatch(loginUser(emailAndPassword))
+    const login = (userData) => dispatch(loginUser(userData))
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault()
+        login(credentials)
+    }
+
+    const handleChange = (ev) => {
+        const { name, value } = ev.target
+        setCredentials({ ...credentials, [name]: value })
+    }
 
     return (
-        <Card className="p-5">
-            <h3>Log In to your account</h3>
-            <LoginForm action={login_user} />
-            <Link to="/signup" className="pt-3">
-                Don't have an account? Signup here
-            </Link>
-        </Card>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Log In
+                </Typography>
+                <form className={classes.form} onSubmit={handleSubmit} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                onChange={handleChange}
+                                value={email}
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                onChange={handleChange}
+                                value={password}
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                        </Grid>
+                    </Grid>
+                    <FormHelperText defaultValue="Error" error id="my-helper-text">
+                        {error}
+                    </FormHelperText>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Log In
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Link href="/signup" variant="body2">
+                                Don't have an account? Signup here
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
     )
 }
 
--- src/pages/signup/signup-page.component.jsx
+++ src/pages/signup/signup-page.component.jsx
@@ -1,25 +1,118 @@
-import React from 'react'
-import LoginForm from '../../components/forms/login-form.component'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { registerUser } from '../../redux/user/user.actions'
-import { useDispatch } from 'react-redux'
-import { Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import { useStyles } from './signup-page.styles'
+import { FormHelperText } from '@material-ui/core'
 
 const Signup = () => {
-    const dispatch = useDispatch()
+    const classes = useStyles()
+    const error = useSelector((state) => state.userAuth.error)
 
-    const signup_user = (username, password, setError) => {
-        dispatch(registerUser(username, password, setError))
+    const [credentials, setCredentials] = useState({
+        displayName: '',
+        email: '',
+        password: ''
+    })
+
+    const { displayName, email, password } = credentials
+
+    const dispatch = useDispatch()
+    const signUp = (userData) => dispatch(registerUser(userData))
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault()
+        signUp(credentials)
+    }
+
+    const handleChange = (ev) => {
+        const { name, value } = ev.target
+        setCredentials({ ...credentials, [name]: value })
     }
 
     return (
-        <Card className="p-5">
-            <h3>Create new account</h3>
-            <LoginForm action={signup_user} />
-            <Link to="/login" className="pt-3">
-                Already have an account? Login here
-            </Link>
-        </Card>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <form className={classes.form} onSubmit={handleSubmit} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                fullWidth
+                                onChange={handleChange}
+                                value={displayName}
+                                id="displayName"
+                                label="Name"
+                                name="displayName"
+                                autoComplete="dname"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                onChange={handleChange}
+                                value={email}
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                onChange={handleChange}
+                                value={password}
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                        </Grid>
+                    </Grid>
+                    <FormHelperText defaultValue="Error" error id="my-helper-text">
+                        {error}
+                    </FormHelperText>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Link href="/login" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
     )
 }
 
